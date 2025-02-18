fc54283a59e021afeeb99eb450ce4d1990dc91f7
Aditya Sharma <aditya22950@gmail.com>
2025-02-19 03:48:20 +0530

    pushed code

--- pet-care/src/Pages/Home.jsx
+++ pet-care/src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Hero from '../components/heroSection/hero'
+import Hero from '../components/heroSection/Hero'
 import Services from '../components/ourServices/Services'
 import Cta from '../components/cta/Cta'
 import DoctorsSlider from '../components/sliders/DoctorsSlider'
